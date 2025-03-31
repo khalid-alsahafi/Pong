9d100dce39c2d87517bd3b3cff4a5db9ad052705
khalid-alsahafi <khallshock@gmail.com>
2025-03-31 08:45:48 +0300

    Added sounds

--- core/src/main/java/pong/Pong/Main.java
+++ core/src/main/java/pong/Pong/Main.java
@@ -2,13 +2,19 @@ package pong.Pong;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import java.util.ArrayList;
-import java.util.Random;
 
-
+/*
+TODO:
+    - Add levels
+    - Add win screen
+DONE:
+    - Add sounds
+ */
 
 public class Main extends ApplicationAdapter {
 
@@ -16,6 +22,9 @@ public class Main extends ApplicationAdapter {
     Ball ball;
     Paddle paddle;
     ArrayList<Block> blocks = new ArrayList<>();
+    Sound brickSound, loseSound, paddleSound, winSound;
+    String winMessage = "You Win!";
+    Boolean lost = false;
 
     @Override
     public void create() {
@@ -23,22 +32,27 @@ public class Main extends ApplicationAdapter {
         // make ball
         int ballX = Gdx.graphics.getWidth() / 2 - 200;
         int ballY = Gdx.graphics.getHeight() / 2;
-        int xSpeed = 4;
-        int ySpeed = 5;
-        ball = new Ball(ballX, ballY, 10, xSpeed, ySpeed);
+        int ballXSpeed = 4;
+        int ballYSpeed = 5;
+        ball = new Ball(ballX, ballY, 10, ballXSpeed, ballYSpeed);
         // make paddle
         paddle = new Paddle(Gdx.graphics.getWidth()/2, 20, 100, 10);
 
         // make bricks
-        int rows = 6;
-        int cols = 8;
-        for (int row = 0; row < rows; row++) {
-            for (int column = 0; column < cols; column++) {
-                int width = (Gdx.graphics.getWidth() - 5 * cols) / cols;
+        int brickRows = 6;
+        int brickColumns = 8;
+        for (int row = 0; row < brickRows; row++) {
+            for (int column = 0; column < brickColumns; column++) {
+                int width = (Gdx.graphics.getWidth() - 5 * brickColumns) / brickColumns;
                 int height = 20;
                 blocks.add(new Block(column * (width + 5), Gdx.graphics.getHeight() - 25 - row * (height + 5), width, height));
             }
         }
+        brickSound = Gdx.audio.newSound(Gdx.files.internal("audio/brick.mp3"));
+        System.out.println(System.getProperty("user.dir"));
+        paddleSound = Gdx.audio.newSound(Gdx.files.internal("audio/paddle.mp3"));
+        loseSound = Gdx.audio.newSound(Gdx.files.internal("audio/lose.mp3"));
+        //winSound = Gdx.audio.newSound(Gdx.files.internal("audio/win.mp3"));
     }
 
     @Override
@@ -53,6 +67,7 @@ public class Main extends ApplicationAdapter {
         ball.checkCollision(paddle);
         // ball-paddle collision
         if (ball.collidesWith(paddle)) {
+            paddleSound.play();
             int distToPaddleCenter;
             int paddleCenter = paddle.x + paddle.width/2;
             distToPaddleCenter = ball.x - paddleCenter;
@@ -66,6 +81,7 @@ public class Main extends ApplicationAdapter {
         // ball-block collision loop
         for (int i = 0; i < blocks.size(); i++) {
             if (ball.collidesWith(blocks.get(i))) {
+                brickSound.play();
                 // right-left collision
                 if (ball.y > blocks.get(i).y && ball.y < blocks.get(i).y + blocks.get(i).height &&
                     !(ball.x > blocks.get(i).x && ball.x < blocks.get(i).x + blocks.get(i).width)) {
@@ -77,6 +93,10 @@ public class Main extends ApplicationAdapter {
                 }
                 blocks.remove(blocks.get(i));
             }
+        }
+        if (ball.y - ball.size < 0 && !lost) {
+            loseSound.play();
+            lost = true;
         }
         // draw objects
         ball.draw(shape);
